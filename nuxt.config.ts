d06ef43727fa0ab47c47b2f60fd5009dd2223f2a
tomidev23 <tomingselingga2512@gmail.com>
2026-02-22 00:30:51 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
+function defineNuxtConfig(arg0: { compatibilityDate: string; devtools: { enabled: boolean; }; }) {
+  throw new Error("Function not implemented.");
+}
+
